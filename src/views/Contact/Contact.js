88026909b95f6d2972de558270ba719ef114b693
xuanphu2701 <46343756+xuanphu2701@users.noreply.github.com>
2022-11-15 09:58:32 +0700

    add contact

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -14,7 +14,83 @@ import { AiFillPhone } from "react-icons/ai";
 
 class Contact extends Component {
   render() {
-    return <div>Content HTML</div>;
+    return (
+      <div className="container">
+        <div className="section-header">
+          <HomeHeader></HomeHeader>
+        </div>
+        <section id="sidebar">
+          <p>
+            <NavLink to="/" style={{ color: "black" }}>
+              Trang chủ
+            </NavLink>{" "}
+            | <b>Liên hệ</b>
+          </p>
+        </section>
+        <div className="container-contact">
+          <h1 className="title-contact">THÔNG TIN LIÊN HỆ UITBOOK</h1>
+          <div className="content-contact">
+            <h3 className="title-2">Ghé thăm UITbook</h3>
+            <div className="col">
+              <div className="col-1">
+                <a href="https://twitter.com/">
+                  <AiFillTwitterCircle color="1DA1F2" fontSize="5em" />
+                </a>
+              </div>
+              <div className="col-2">
+                <a href="https://www.facebook.com/">
+                  <FaFacebook color="2f55a4" fontSize="4.3em" />
+                </a>
+              </div>
+              <div className="col-3">
+                <a href="https://www.youtube.com/">
+                  <BsYoutube
+                    className="youtube"
+                    color="FF0000"
+                    fontSize="5em"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="second">
+              <h5>Thông tin chi tiết</h5>
+              <div className="second-col">
+                <div className="tit">
+                  <FaFacebook />
+                  <b> FACEBOOK : </b>
+                  <span>https://www.facebook.com/UITPHONE</span>
+                  <br />
+                  <AiFillInstagram />
+                  <b> INSTARGRAM : </b>
+                  <span>https://www.instagram.com/uitphone_vietnam/</span>
+                  <br />
+                  <AiFillShopping />
+                  <b> SHOPPE : </b>
+                  <span>https://shopee.vn/uitphone_vietnam</span>
+                  <br />
+                </div>
+                <div className="tit-2">
+                  <AiTwotoneHome />
+                  <span> UIT, Linh Trung, Thủ Đức, Hồ Chí Minh</span>
+                  <br />
+                  <MdEmail />
+                  <b> EMAIL : </b>
+                  <span>uitphone@gmail.com</span>
+                  <br />
+                  <AiFillPhone />
+                  <b> Hotline : </b>
+                  <span>01 234 567 88</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-2">
+          <Footer />
+        </div>
+      </div>
+    );
   }
 }
 
